f77ccdced32ead04542df8e580162d1f0c8a1d5b
tsupol <ton.supol@gmail.com>
2016-10-16 20:13:41 +0700

    fix package.json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 // export Hello from './greetings/Hello';
 export Hola from './greetings/Hola';
-// export Loading from './widgets/Loading';
+export Loading from './widgets/Loading';
+export SemiThemeProvider from './main/SemiThemeProvider';
